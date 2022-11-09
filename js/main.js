3c8b1ad40d4e3d2258f632ec96551548bb4e4337
ayoungpark <ayoungparkme@gmail.com>
2022-11-09 10:30:14 +0900

    adding work

--- js/main.js
+++ js/main.js
@@ -157,3 +157,32 @@ window.addEventListener('wheel', () => {
   }
   selectNavItem(navItems[selectedNavIndex]);
 });
+
+// Params
+const j$ = jQuery,
+    $mContainer = j$(".mnsry_container"),
+    $filterButton = j$(".button"),
+    $loadingMessage = j$(".loading_msg");
+    $params = {
+      itemSelector: ".item",
+      filtersGroupSelector:".filters"
+    // Uncomment below to set the selectorType to use <ul> instead of inputs
+    // selectorType: "list"
+    };
+
+// After the page is loaded
+j$(window).load(function() { 
+  // Do mansonry with filtering 
+  $mContainer.multipleFilterMasonry($params);
+  // Show articles with fadein
+  $mContainer.find("article").animate({
+      "opacity":1
+    }, 1200);
+  // Hide loading message
+  $loadingMessage.fadeOut();
+  
+  // Change the filtering button(label) status 
+  $filterButton.find("input").change(function(){
+    j$(this).parent().toggleClass("active");
+  });
+});
